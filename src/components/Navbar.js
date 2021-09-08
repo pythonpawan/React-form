3c1e244945f4a5ce06139ef1a868ed6c51962dc5
PAWAN KUMAR <pawan>
2021-09-08 19:49:34 +0700

    Add all my files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import React from 'react'
     return (
         <div>
             <>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-light bg-dark">
   <div className="container-fluid">
     <a className="navbar-brand" href="/">Registration site</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
